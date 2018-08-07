9650e7551e01f7c7d62d5fc7e50c62fdbbdedf09
Jamal Dawson <jamal.a.dawson@gmail.com>
2018-08-07 12:48:08 -0400

    lottery kata

---
+++ Kata/Lottery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class Lottery
+
+    {
+        List<int> LotteryNumbers;
+
+        public Lottery(List<int> lotteryNumbers)
+        {
+            this.LotteryNumbers = lotteryNumbers;
+        }
+        public Lottery()
+        {
+            LotteryNumbers = new List<int>();
+        }
+        public List<int> GetLotteryNumbers()
+        {
+            LotteryNumbers.Sort();
+            return LotteryNumbers;
+        }
+        public void DrawNumber(int RandomNumber)
+        {
+       
+            LotteryNumbers.Add(RandomNumber);
+            
+        }
+        public void BubbleSort()
+        {
+            
+        }
+    }
+}
---
+++ KataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("baa35552-b5f2-4801-926c-d42bdcb5c662")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
